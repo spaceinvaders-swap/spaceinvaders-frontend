4bcfaca0edad574bbf8602157358ee18401c5c74
phunkatrunc <phunkatrunc@gmail.com>
2023-03-03 20:07:58 +0530

    commit

--- apps/aptos/components/Pools/components/PoolCard/CakeCardActions.tsx
+++
@@ -1,9 +0,0 @@
-import { Pool } from '@spaceinvaders-swap/uikit'
-import { Coin } from '@spaceinvaders-swap/aptos-swap-sdk'
-import InvaCollectModal from './InvaCollectModal'
-import InvaStakeModal from './InvaStakeModal'
-
-const HarvestActions = Pool.withCollectModalCardAction(InvaCollectModal)
-const StakeActions = Pool.withStakeActions(InvaStakeModal)
-
-export default Pool.withCardActions<Coin>(HarvestActions, StakeActions)
--- packages/uikit/src/components/CakePrice/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as InvaPrice } from "./InvaPrice";
-export type { Props as InvaPriceProps } from "./InvaPrice";
